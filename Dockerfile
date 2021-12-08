3e98d0b800d082f5b2e6766a2b4f0f3f30b4c7f4
ludo <ludovico.funari@gmail.com>
2021-12-08 23:34:15 +0100

    workig with spark

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,5 @@
 FROM docker.io/bitnami/spark:3.2.0-debian-10-r3
 
+COPY spark-executor.py /opt/bitnami/spark/examples/spark-executor.py
+
 USER root
---
+++ spark-executor.py
@@ -0,0 +1,26 @@
+import os
+import subprocess
+import sys
+
+
+try:
+    hostname = os.environ['HOSTNAME']
+    start = "_".join(hostname.split("-")[0:2]).upper()
+    end = "7077_TCP_ADDR"
+    port = 7077
+    print(start)
+    # exit()
+
+    for key, value in os.environ.items():
+        if key.startswith(start) and key.endswith(end):
+            spark_ep = f"{value}:{port}"
+            print(f"spark_endpoint ----> {spark_ep}")
+
+    spark_command = sys.argv[1].split(" ")
+    spark_command.insert(1, "--master")
+    spark_command.insert(2, f"spark://{spark_ep}")
+    print(f"####### COMMAND: {spark_command}")
+    
+    process = subprocess.run(spark_command)
+except Exception as err:
+    print(err)
